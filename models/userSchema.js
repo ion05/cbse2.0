085282fd41ef024135595b21606ad071dcfe2f9a
AlightKnight343 <thegodofgreatness343@gmail.com>
2021-08-19 14:41:17 +0530

    authentication done

---
+++ models/userSchema.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const reqString = { type:String, required:true }
+const moment = require("moment")
+let now = new Date()
+let dateStringWithTime = moment(now).format('YYYY-MM-DD HH:MM:SS');
+
+const userSchema = new mongoose.Schema({
+    email:reqString, 
+    username:reqString,
+    password:reqString,
+    date: {
+        type:String,
+        default: dateStringWithTime
+    },
+})
+
+userSchema.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model("User", userSchema)
